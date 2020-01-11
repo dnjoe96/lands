0d6501b054ccd1047a9aa80443d7bbc6cb26fd15
dnjoe96 <donjoedbest@gmail.com>
2020-01-11 23:08:54 +0100

    removed unneccesary codes

--- app/routes.py
+++ app/routes.py
@@ -43,39 +43,3 @@ def transaction(sourceID):
     return render_template('table.html', result=result2, id=sourceID)
 
 
-
-@app.route('/user/<string:id>', methods=['GET', 'POST'])
-def index_id(id):
-
-    print(id)
-    table1meta = MetaData(engine)
-    table1 = Table('lr_process_history', table1meta, autoload=True)
-    DBSession = orm.sessionmaker(bind=engine)
-    session = DBSession()
-    # results = session.query(table1).order_by(table1.columns.task_create).all()
-    search_id = '100000000000'+str(id)
-    print(search_id)
-    results = session.query(table1).filter(table1.columns.id==int(search_id)).first()
-
-    if results:
-        # print(results)
-        return render_template('index.html', results=results)
-    flash('id {} does not exist'.format(search_id), 'success')
-    return redirect(url_for('index'))
-
-
-@app.route('/transaction/<string:sourceID>')
-def index2(sourceID):
-    table1meta = MetaData(engine)
-
-    table1 = Table('lr_transaction', table1meta, autoload=True)
-    table2 = Table('lr_process_history', table1meta, autoload=True)
-
-    DBSession = orm.sessionmaker(bind=engine)
-    session = DBSession()
-
-    transaction = session.query(table1).filter(table1.columns.source_property_ids==sourceID).first()
-    process = session.query(table2).filter(table2.columns.transaction_id==transaction.id).first()
-
-    print(process)
-    return render_template('index.html', results=process)
