4bdf34d5332641fb70d83b639e912b2f7b25de59
dnjoe96 <donjoedbest@gmail.com>
2020-01-14 09:29:11 +0100

    uninstalled and removed dotenv module

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
-from dotenv import load_dotenv
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
 
 # creating a configuration class
-
-
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'default'
     # 'mssql+pyodbc://<username>:<password>@<Host>:<Port>/LendApp'
