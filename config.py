2a6275fbbab397011388f77160417a9aa5845a2a
dnjoe96 <donjoedbest@gmail.com>
2020-01-15 10:45:27 +0100

    further correction

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+# install dotenv and python-dotenv
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 # creating a configuration class
